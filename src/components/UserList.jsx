5dd5fede2e69d7ade14d69e84ab80c04d34252cf
Gopalpawar <kp015770gmail.com>
2026-01-16 19:00:23 +0530

    useEffect to fetch user data from an API and display it in a UserList component.jsx

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState   } from "react";
+import { useEffect, useState } from "react";
 
 function Userlist() {
     // need something to store the data
@@ -6,28 +6,29 @@ function Userlist() {
 
     useEffect(() => {
         // fetch data from the API
-       const fetchUsers = async () => {
-        try {  
+        const fetchUsers = async () => {
+            try {
 
-        const response = await fetch('https://jsonplaceholder.typicode.com/users');
+                const response = await fetch('https://jsonplaceholder.typicode.com/users');
 
-        if (!response.ok) {
-            throw new Error('something went wrong'+ response.status);
+                if (!response.ok) {
+                    throw new Error('something went wrong' + response.status);
 
+                }
+
+                const data = await response.json();
+                setUsers(data);
+
+            }
+            catch (error) {
+
+                console.error(error.message);
+
+            }
         }
+        fetchData();
+       }   , [])
 
-        const data = await response.json();
-        setUsers(data);
 
-       }
-       catch (error) {
-
-        console.error( error.message);
-       
-    }
-    }
-        ,[]);
-
- 
 }
 export default Userlist;

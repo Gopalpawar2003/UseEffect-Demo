527b3fb8f368c4bcaf970aca30f83a30f0fc1d8b
Gopalpawar <kp015770gmail.com>
2026-01-16 18:48:39 +0530

    node installed on your machine. project craeted using vite + react template. folder craeted with name UseEffect-demo in Desktop React-projects Initial commit of UseEffect-demo Add initial project files to UseEffect-demo

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  // use effect we need to fetch user from an API https://jsonplaceholder.typicode.com/users
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/UserList.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState   } from "react";
+
+function Userlist() {
+    // need something to store the data
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        // fetch data from the API
+       const fetchUsers = async () => {
+        try {  
+
+        const response = await fetch('https://jsonplaceholder.typicode.com/users');
+
+        if (!response.ok) {
+            throw new Error('something went wrong'+ response.status);
+
+        }
+
+        const data = await response.json();
+        setUsers(data);
+
+       }
+       catch (error) {
+
+        console.error( error.message);
+       
+    }
+    }
+        ,[]);
+
+ 
+}
+export default Userlist;

51ba90095b48d2d35be1150c3bbede6c40f17b3e
Gopalpawar <kp015770gmail.com>
2026-01-16 19:26:31 +0530

    post list function created for inheriting the data

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import Userlist from './components/UserList.jsx'
+import Postlist from './components/PostList.jsx'
 
 function App() {
   // use effect we need to fetch user from an API https://jsonplaceholder.typicode.com/users
   return (
     <>
       <Userlist />
+      <Postlist />
     </>
   )
 }
---
+++ src/components/PostList.jsx
@@ -0,0 +1,41 @@
+// fetch and display list of posts from given API
+// https://jsonplaceholder.typicode.com/posts.
+import { useEffect, useState } from "react";
+
+function PostList() {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+
+            try {
+                const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+                if (!response.ok) {
+                    throw new Error('something went wrong' + response.status);
+                }
+                const data = await response.json();
+                setPosts(data);
+            } catch (error) {
+                console.error(error.message);
+            }
+        };
+        fetchPosts();
+    }, []);
+
+return (
+    <>
+        <h2 className="heading">Posts</h2>  
+        <div className="container">
+            {posts.map((post) => {
+                return (
+                    <div id={post.id} className="card">
+                        <h3>{post.title}</h3>
+                        <p>{post.body}</p>
+                    </div>
+                );
+            })}
+        </div>
+    </>
+);
+}
+export default PostList;
